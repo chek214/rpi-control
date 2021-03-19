b25dee514aac74c22bf74a8d2b8a9a2bbad82cec
chek <chek214@live.com>
2021-03-19 16:11:44 -0600

    background.jpg

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ http.listen(80); //listen to port 80
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  fs.readFile(__dirname + '/public/index.htm', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-});
+// app.get('/', (req, res) => {
+//   fs.readFile(__dirname + '/public/index.htm', function(err, data) { //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+//     res.write(data); //write data from index.html
+//     return res.end();
+//   });
+// });
 
 // function handler (req, res) { //create server
 //   fs.readFile(__dirname + '/public/index.htm', function(err, data) { //read file index.html in public folder
