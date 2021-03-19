a73c3a57bc89d45a240cdec6f85b62822532c613
chek <chek214@live.com>
2021-03-19 16:27:15 -0600

    background.jpg

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
+//var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-http.listen(80); //listen to port 80
 
-app.use(express.static('public'));
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+
+
+var server = app.listen(5000);
+
 
 // app.get('/', (req, res) => {
 //   fs.readFile(__dirname + '/public/index.htm', function(err, data) { //read file index.html in public folder
