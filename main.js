cb59be946c2196162400c607baf6ea7b55a936b8
chek <chek214@live.com>
2021-03-19 16:57:04 -0600

    background.jpg

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var public = path.join(__dirname, 'public');
 
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(public, 'index.html'));
+  res.sendFile(path.join(public, 'index.htm'));
 });
 
 app.use('/', express.static(public));
