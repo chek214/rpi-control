c7218b1697b0ae0e67e718f79d3ad6ad65e801be
chek <chek214@live.com>
2021-03-25 15:35:15 -0600

    error 5 socket io

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
-var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var io = require('socket.io')(http, {
+  allowEIO3: true 
+})
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
