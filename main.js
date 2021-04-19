47ad93f1ed95cdfa3b87fc6a17ab7cf50db1bb33
chek <chek214@live.com>
2021-04-19 15:08:25 -0500

    fs writefile

--- main.js
+++ main.js
@@ -104,7 +104,8 @@ io.sockets.on('connection', function (socket) {
     fs.writeFile('configs.json', JSON.stringify(configs), function (err) {
       if (err) return console.log(err)
       console.log('saved')
-      console.log(configs[0])
+      console.log(configs[1])
+      console.log(configs[2])
     }) 
   })
 
